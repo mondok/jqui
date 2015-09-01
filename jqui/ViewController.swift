978de714db415b92e9aaf9b82de0cf4abbe91525
Matt Mondok <matt.mondok@gmail.com>
2015-08-31 20:14:52 -0400

    Initial Commit

---
+++ jqui/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  jqui
+//
+//  Created by Matthew Mondok on 8/31/15.
+//  Copyright (c) 2015 Matthew Mondok. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
